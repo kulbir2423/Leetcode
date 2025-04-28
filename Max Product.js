3d33d2bf6b8d42fe774eb84e55740b8d5a4fa751
kulbir2423 <kulbir.singh1996@gmail.com>
2025-04-28 22:16:32 +0530

    Code Completed

---
+++ Max Product.js
@@ -0,0 +1,9 @@
+let nums8 = [1,2,3];
+let product = 0;
+let length = nums8.length;
+var maximumProduct = function (nums) {
+  nums.sort((a, b) => a - b);
+  console.log(nums); 
+  product = product + nums8[length]*nums8[length-1]*nums8[length-2];
+};
+maximumProduct(nums8);
